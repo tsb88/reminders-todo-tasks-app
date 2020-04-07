f81314452b4c40947416814173fa1c2a414b338b
tsb88 <tejasbhoir23@gmail.com>
2020-04-07 11:30:12 -0400

    added delete functions that were accidentally deleted from previous branch

--- src/TodoItems/TodoItems.js
+++ src/TodoItems/TodoItems.js
@@ -2,18 +2,29 @@ import React, { Component } from "react";
 import "./TodoItems.css";
 
 class TodoItems extends Component {
+
+    constructor(props) {
+        super(props);
+     
+        this.createTasks = this.createTasks.bind(this);
+    }
+
     createTasks(item) {
-        return <div className="header">
-                <li key={item.key}>
+        return <div key={item.key} className="header">
+                <li>
                     {item.text}
                 </li>
                 <button>
                     Edit
                 </button>
-                <button>
+                <button onClick={() => this.delete(item.key)}>
                     Delete
                 </button>
             </div>
+    }
+
+    delete(key) {
+        this.props.delete(key);
     }
 
     render() {
--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -11,6 +11,7 @@ class TodoList extends Component {
         };
 
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
 
     addItem(e) {
@@ -34,6 +35,16 @@ class TodoList extends Component {
         e.preventDefault();
     }
 
+    deleteItem(key) {
+      var filteredItems = this.state.items.filter(function (item) {
+        return (item.key !== key);
+      });
+
+      this.setState({
+        items: filteredItems
+      });
+    }
+
     render() {
       return (
         <div className="todoListMain">
@@ -46,7 +57,8 @@ class TodoList extends Component {
               </button>
             </form>
           </div>
-          <TodoItems entries={this.state.items}/>
+          <TodoItems entries={this.state.items}
+          delete={this.deleteItem}/>
         </div>
       )
     }

de5e979f1e9c24258cd03766545841c47f647d73
tsb88 <tejasbhoir23@gmail.com>
2020-04-27 19:13:29 -0400

    updated config file

--- src/config.js
+++ src/config.js
@@ -1,17 +1,43 @@
+const dev = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "tasks-app-2-api-dev-attachmentsbucket-1emxwx4yorais"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://nl4ftbn88k.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_AGfeWCwDh",
+    APP_CLIENT_ID: "116id59j3sj0ngu4bq0m3ou19g",
+    IDENTITY_POOL_ID: "us-east-1:8ac7952a-4150-4d84-a893-db152dcd1d8c"
+  }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "tasks-app-2-api-prod-attachmentsbucket-rhjgh9yal6l5"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://6lbbbq23ug.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_yjmjOgRMe",
+    APP_CLIENT_ID: "32s7brf2ficppj0sft3omu5kdl",
+    IDENTITY_POOL_ID: "us-east-1:badcea45-d9f1-4a10-bf33-a1c6583c13b3"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
 export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "reminders-todo-tasks-app"
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://gn1hy07fpi.execute-api.us-east-1.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_GVpnPuULV",
-      APP_CLIENT_ID: "2lh6t0hbttl9t4ib2qgmj1d8mo",
-      IDENTITY_POOL_ID: "us-east-1:7bc5268c-1e7b-413e-9cc4-683c54c2ac85"
-    }
-  };
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
+};
